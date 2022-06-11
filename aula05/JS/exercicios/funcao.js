ed06ee486a9db4eb577e6c9055735c2c589287d8
Robson-SM <profes_robson@hotmail.com>
2022-06-11 10:30:12 -0300

    Adcionando aula07

---
+++ aula05/JS/exercicios/funcao.js
@@ -0,0 +1,28 @@
+subtrair
+function somar(){
+//sem paramêtros
+var n1 = 5
+var n2 = 15
+
+console.log(`A soma entre: ${n1} + ${n2} = ${n1 = n2}`)
+
+}
+
+function somaComParametros (num1, num2){
+
+// com parêmtros
+
+console.log (`A soma entre: ${num1} + ${num2} = ${num1 + num2}`)
+}
+
+var subtrair = () => {
+// arrow function sem parâmetros
+console.log (`Hello arrow function`)
+
+var dividir = (n1, n2) => {
+// arrow function sem parâmetros
+console.log (n1/ n2)
+
+}
+
+}
---
+++ aula05/JS/exercicios/funcao2.js
@@ -0,0 +1,13 @@
+function imprimeNome(pessoa){
+    console.log(pessoa.nome)
+
+}
+
+var p1 = {
+
+    nome: `Miguel`
+}
+
+imprimeNome(p1)
+
+
